1fb3346c4780ec739270af8506cc1b060372a319
mumuja <33843345+mumuja@users.noreply.github.com>
2026-02-12 11:42:31 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Be My Valentine</title>
+		<link rel="stylesheet" href="style.css" />
+		<link rel="shortcut icon" href="./assets/heart.svg" type="image/x-icon" />
+		<link rel="preconnect" href="https://fonts.googleapis.com" />
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+		<link
+			href="https://fonts.googleapis.com/css2?family=Loved+by+the+King&display=swap"
+			rel="stylesheet" />
+		<link
+			href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+			rel="stylesheet" />
+		<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+		<link
+			rel="stylesheet"
+			href="https://site-assets.fontawesome.com/releases/v6.4.2/css/all.css" />
+		<script src="https://smtpjs.com/v3/smtp.js"></script>
+	</head>
+	<body>
+		<div class="preloader">
+			<lottie-player
+				src="./assets/heart.json"
+				background="transparent"
+				speed="1"
+				style="width: 300px; height: 300px"
+				loop
+				autoplay></lottie-player>
+		</div>
+		<nav class="top-nav">
+			<img src="./assets/heart.svg" alt="" />
+			<h1>Be My Valentine</h1>
+			<img src="./assets/heart.svg" alt="" />
+		</nav>
+		<div class="form-content">
+			<form action="" class="valentine-form" netlify>
+				<div class="input-field">
+					<input
+						type="text"
+						name="username"
+						placeholder=""
+						id="name"
+						autocomplete="off" />
+					<label class="placeholder">Your Name</label>
+				</div>
+				<div class="input-field">
+					<input
+						type="email"
+						name="username"
+						placeholder=""
+						id="email"
+						autocomplete="off" />
+					<label class="placeholder">Your Email</label>
+				</div>
+				<div class="input-field">
+					<input
+						type="text"
+						name="username"
+						placeholder=""
+						id="valentineName"
+						autocomplete="off" />
+					<label class="placeholder">Your Valentine Name</label>
+				</div>
+				<button type="submit">Create Link</button>
+			</form>
+		</div>
+		<div id="toast">
+			<i id="img" class="fa-solid fa-heart"></i>
+			<div id="desc">A notification message..</div>
+		</div>
+		<div class="link-area">
+			<div class="link-content">
+				<h2>Your Valentine Link</h2>
+				<a href="" target="_blank">Please share this link to your valentine</a>
+			</div>
+			<p>Response will be sent in your email</p>
+			<button id="copyToClipboard">
+				<i class="fa-solid fa-clipboard"></i>
+				<span>Copy</span>
+			</button>
+		</div>
+		<div class="creator-contact">
+			<h3>
+				Created with <i class="fa-solid fa-heart" style="color: #f1356d"></i> by
+				AAKASH DHAKAL
+			</h3>
+			<ul class="links">
+				<li>
+					<a href="https://github.com/aakashdhakal" target="_blank">
+						<i class="fa-brands fa-github"></i>
+					</a>
+				</li>
+				<li>
+					<a
+						href="https://www.linkedin.com/in/aakash-dhakal12/"
+						target="_blank">
+						<i class="fa-brands fa-linkedin"></i>
+					</a>
+				</li>
+				<li>
+					<a href="https://twitter.com/aakash_dhakal12" target="_blank">
+						<i class="fa-brands fa-x-twitter"></i>
+					</a>
+				</li>
+				<li>
+					<a href="https://instagram.com/aakash_dhakal12" target="_blank">
+						<i class="fa-brands fa-instagram"></i>
+					</a>
+				</li>
+			</ul>
+		</div>
+		<dialog id="invitation">
+			<div class="invitation-content">
+				<div class="illustration">
+					<img src="assets/promise-day.svg" alt="" />
+				</div>
+				<div class="invitation">
+					<h1>
+						<img src="./assets/heart.svg" alt="" />
+						Hello,<span class="crush">name</span>
+						<img src="./assets/heart.svg" alt="" />
+					</h1>
+					<p>
+						Hey <span class="crush">name</span>, do you have a map? Because I
+						just got lost in your profile picture. Now that I've found my way,
+						<b>Would you be my Valentine? </b>😊<br />
+						Sent with
+						<i class="fa-solid fa-heart" style="color: #f1356d"></i> by
+						<span class="creator">Name</span>
+					</p>
+
+					<div class="yes-no">
+						<button id="yes">Yes 😊</button>
+						<button id="no">No 🥺</button>
+					</div>
+				</div>
+			</div>
+		</dialog>
+		<script src="script.js"></script>
+	</body>
+</html>
